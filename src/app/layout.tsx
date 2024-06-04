7c0609f4fa67b3c05f08a344a987407674c025d7
Max Murakami-Moses <110478230+maxMurakamiMoses@users.noreply.github.com>
2024-06-04 20:14:45 +0900

    added intro

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,8 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "Max | Personal Website",
+  description: "Max's Personal Website",
 };
 
 export default function RootLayout({
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,12 +4,13 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2030 Ricardo. All rights reserved.
+        &copy; 2024 Max. All rights reserved.
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
         React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
-        Framer Motion, React Email & Resend, Vercel hosting.
+        Framer Motion, React Email & Resend, Fleek hosting. You can visit it at 
+https://max.on-fleek.app or https://maxmurakami.eth.limo/.
       </p>
     </footer>
   );
