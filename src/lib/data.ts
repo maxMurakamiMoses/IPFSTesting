1eee4da2ed38b6c4d34670e78d7f6f522cc23af5
Max Murakami-Moses <110478230+maxMurakamiMoses@users.noreply.github.com>
2024-06-04 23:50:52 +0900

    porjects

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,9 +2,9 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+// import corpcommentImg from "@/public/corpcomment.png";
+// import rmtdevImg from "@/public/rmtdev.png";
+// import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
   {
@@ -60,29 +60,40 @@ export const experiencesData = [
   },
 ] as const;
 
-// export const projectsData = [
-//   {
-//     title: "CorpComment",
-//     description:
-//       "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-//     tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-//     imageUrl: corpcommentImg,
-//   },
-//   {
-//     title: "rmtDev",
-//     description:
-//       "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-//     tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-//     imageUrl: rmtdevImg,
-//   },
-//   {
-//     title: "Word Analytics",
-//     description:
-//       "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-//     tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-//     imageUrl: wordanalyticsImg,
-//   },
-// ] as const;
+export const projectsData = [
+  {
+    title: "Patient Managment System",
+    description:
+      "Built Duke Sport Performance Medical Center's patient managment platform. They are actively using the platform.",
+    tags: ["React", "Next.js", "NeonTech", "Tailwind", "Clerk", "Prisma"],
+    imageUrl: "/build.png",
+    link: "https://duke-medical-platform.com",
+  },
+  {
+    title: "BlueCityDAO website",
+    description:
+      "Built BlueCityDAO's website, helping bring in over a million dollars in initial funding for the DAO! ",
+    tags: ["React", "Material-UI", "Javascript"],
+    imageUrl: "/bullcity.png",
+    link: "https://bullcitydao.xyz",
+  },
+  {
+    title: "Colormotion",
+    description:
+      "Colormotion is a mobile app and chrome extension that highlights youtube captions based on the emotion expressed. Won 1st place overall, out of 200+ people!",
+    tags: ["React naitive", "Node.js", "Javascript", "HTML", "CSS"],
+    imageUrl: "/colormotion.png",
+    link: "https://devpost.com/software/emote-8olims",
+  },
+  {
+    title: "Manus-AI",
+    description:
+      "Manus.AI is an web app that translates ASL into text. Got 3rd place on the wellness track out of 100+ people!",
+    tags: ["Python", "Tensorflow", "react", "javascript"],
+    imageUrl: "/manus.png",
+    link: "https://devpost.com/software/manus-ai",
+  },
+] as const;
 
 export const skillsData = [
   "HTML",
