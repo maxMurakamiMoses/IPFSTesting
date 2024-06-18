7134f5b6bb4d6f74412ecc38f2c555b0d1aab769
Max Murakami-Moses <110478230+maxMurakamiMoses@users.noreply.github.com>
2024-06-18 23:05:58 +0900

    fixed metor styling

--- src/components/ui/meteors.tsx
+++ src/components/ui/meteors.tsx
@@ -11,22 +11,25 @@ export const Meteors = ({
   const meteors = new Array(number || 20).fill(true);
   return (
     <div className="absolute inset-0 overflow-hidden">
-      {meteors.map((el, idx) => (
-        <span
-          key={"meteor" + idx}
-          className={cn(
-            "animate-meteor-effect absolute h-0.5 w-0.5 rounded-[9999px] bg-slate-500 shadow-[0_0_0_1px_#ffffff10] rotate-[215deg]",
-            "before:content-[''] before:absolute before:top-1/2 before:transform before:-translate-y-[50%] before:w-[50px] before:h-[1px] before:bg-gradient-to-r before:from-[#64748b] before:to-transparent",
-            className
-          )}
-          style={{
-            top: Math.floor(Math.random() * 100) + "%",
-            left: Math.floor(Math.random() * 100) + "%",
-            animationDelay: Math.random() * (0.8 - 0.2) + 0.2 + "s",
-            animationDuration: Math.floor(Math.random() * (10 - 2) + 2) + "s",
-          }}
-        ></span>
-      ))}
+      {meteors.map((el, idx) => {
+        const startFromLeft = Math.random() > 0.5;
+        return (
+          <span
+            key={"meteor" + idx}
+            className={cn(
+              "animate-meteor-effect absolute h-0.5 w-0.5 rounded-[9999px] bg-slate-500 shadow-[0_0_0_1px_#ffffff10] rotate-[215deg]",
+              "before:content-[''] before:absolute before:top-1/2 before:transform before:-translate-y-[50%] before:w-[50px] before:h-[1px] before:bg-gradient-to-r before:from-[#64748b] before:to-transparent",
+              className
+            )}
+            style={{
+              top: startFromLeft ? Math.floor(Math.random() * 95) + "%" : "-1%",
+              left: startFromLeft ? "-1%" : Math.floor(Math.random() * 95) + "%",
+              animationDelay: Math.random() * (2 - 0.8) + 0.8 + "s",
+              animationDuration: Math.floor(Math.random() * (60 - 20) + 20) + "s",
+            }}
+          ></span>
+        );
+      })}
     </div>
   );
 };
